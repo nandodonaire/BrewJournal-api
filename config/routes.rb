49e5e2952833997857aaf35a7ff69f50b8948b44
nandodonaire <nandodonaire@gmail.com>
2018-03-04 18:31:10 -0500

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  resources :examples, except: %i[new edit]
+  post '/sign-up' => 'users#signup'
+  post '/sign-in' => 'users#signin'
+  delete '/sign-out' => 'users#signout'
+  patch '/change-password' => 'users#changepw'
+  resources :users, only: %i[index show]
+end
