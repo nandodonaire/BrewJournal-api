b356bf4d040230c6c7797758adbaaeccdb7eae98
nandodonaire <nandodonaire@gmail.com>
2018-03-05 11:31:00 -0500

    Added CURL scripts for GET, DELETE, and PATCH
    --Also tested them and they are working. However, all users are able to
    see the breweries added by other users.
    
    Next: Tie the breweries to a particular user so that others can't see
    them.

---
+++ scripts/breweries/update-brewery.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl "http://localhost:4741/breweries/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "brewery": {
+      "name": "'"${NAME}"'",
+      "location": "'"${LOCATION}"'",
+      "beers_tasted": "'"${BEERS}"'",
+      "notes": "'"${NOTES}"'"
+    }
+  }'
+
+echo
